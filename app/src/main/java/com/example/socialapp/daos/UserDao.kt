c3f013665fa72ea047a0beb8f5ae5f3b6df3958c
Aman Singh <aman.yaduvanshi.560@gmail.com>
2022-07-16 17:09:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/socialapp/daos/UserDao.kt
@@ -0,0 +1,29 @@
+package com.example.socialapp.daos
+
+import com.example.socialapp.models.User
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+//import com.google.firebase.firestore.ktx.firestore
+//import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class UserDao {
+
+    private val db = FirebaseFirestore.getInstance()
+    private val usersCollections = db.collection("users")
+
+    fun addUser(user: User?){
+        user?.let {
+            GlobalScope.launch(Dispatchers.IO) {
+                usersCollections.document(user.uid).set(it)
+            }
+        }
+    }
+
+    fun getUserById(uId: String): Task<DocumentSnapshot> {
+        return usersCollections.document(uId).get()
+    }
+}
